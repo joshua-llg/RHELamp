c5a318ef6015ca6de49f395e781339d0649639a8
joshua-llg <77903928+joshua-llg@users.noreply.github.com>
2021-01-26 09:21:46 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,20 +12,20 @@
  sudo systemctl start httpd 
  sudo systemctl start mariadb 
 
-FILE="/home/josh/CLONES/Lampserver/success"
+FILE="/home/success"
 #setup secure mysql database && stop httpd and mariadb
  sudo mysql_secure_installation
 echo ""
 echo ""
 #temporarely stops httpd/mariadb and redirects success input into success file
  sudo systemctl stop httpd
- sudo systemctl stop mariadb 0>> success
+ sudo systemctl stop mariadb 0>> /home/success
 #checks to see if success file exist
 if [[ -f $FILE ]];then
 	 echo -e "  Lamp install was a successful,\nnow make Server script an executable\n    and sudo ./server up/down "
 	 echo ""	 
 fi
 
-rm success
+sudo rm -rf /home/success
 exit
 
